ddef6e4d6b242c134f84919834e9c86bb56b768d
Bystrova Ann <32645809+Ann2827@users.noreply.github.com>
2020-07-12 14:54:29 +0300

    Table logic (#4)
    * selecting a cell
    
    * navigate
    
    * add emitter
    
    * finish base table logic
    
    Co-authored-by: Быстрова Анна <a.bystrova@admitad.pro>

---
+++ src/components/table/TableSelection.js
@@ -0,0 +1,27 @@
+export class TableSelection {
+    static className = 'selected'
+    
+    constructor() {
+        this.group = []
+        this.curren = null
+    }
+    
+    // $el instanceof DOM === true (т к начинается с $)
+    select($el) {
+        this.clear()
+        $el.focus().addClass('selected')
+        this.group.push($el)
+        this.curren = $el
+    }
+    
+    clear() {
+        this.group.forEach($el => $el.removeClass(TableSelection.className))
+        this.group = []
+    }
+    
+    selectGroup($group = []) {
+        this.clear()
+        this.group = $group
+        this.group.forEach($el => $el.addClass(TableSelection.className))
+    }
+}
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,10 +4,30 @@ export class ExcelComponent extends DomListener {
     constructor($root, options={}) {
         super($root, options.listeners)
         this.name = options.name || ''
+        this.emitter = options.emitter
+        this.unsubscribers = []
+        
+        this.prepare()
     }
+    
+    // Настраиваем наш компонент до инит
+    prepare() {
+    
+    }
+    
     // Возвращает шаблон компонента
     toHTML() {
         return ''
+    }
+    
+    // Паттерн фасад
+    $emit(event, ...args) {
+        this.emitter.emit(event, ...args)
+    }
+    
+    $on(event, fn) {
+        const unsub = this.emitter.subscribe(event, fn)
+        this.unsubscribers.push(unsub)
     }
     
     init() {
@@ -16,5 +36,6 @@ export class ExcelComponent extends DomListener {
     
     destroy() {
         this.removeDomListeners()
+        this.unsubscribers.forEach(unsub => unsub())
     }
 }
